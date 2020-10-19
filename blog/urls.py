27908a3b4984f7e10263afbe0eb4833819c49423
FarishtaJayas <farishtajayas@gmail.com>
2020-10-19 21:04:30 +0600

    Blog Details page made

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.all_blogs, name='all_blogs')
+    path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
